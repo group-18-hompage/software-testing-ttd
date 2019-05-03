6ddde0d27501ffd6611e71bec2582ee5c58c84cc
yeounu <123dusdn@naver.com>
2019-05-03 23:03:51 +0900

    change calculator.java

--- bankmgnt/src/main/java/calculator.java
+++ bankmgnt/src/main/java/calculator.java
@@ -1,9 +1,11 @@
 public class calculator {
     private int result = 0;
+    private double answer = 0;
 
     public int getResult() {
         return result;
     }
+    public double getAnswer() {return answer; }
     public int add(int input1, int input2) {
         return result = (input1 + input2);
     }
@@ -16,4 +18,6 @@ public class calculator {
     public int divide(int input1, int input2) {
         return result = (input1 / input2);
     }
+    public int volume(int hor, int ver, int hei) { return result = (hor * ver * hei);}
+    public double volume(int r) {return answer = (3.14 * r * r);}
 }
