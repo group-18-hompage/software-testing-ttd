3a07b5f1e15a4d6cc69e188ce678c00464e96a41
yeounu <123dusdn@naver.com>
2019-05-03 23:05:56 +0900

    change calculatorTest.java

--- bankmgnt/src/test/java/calculatorTest.java
+++ bankmgnt/src/test/java/calculatorTest.java
@@ -12,31 +12,35 @@ public class calculatorTest {
     }
 
     @Test //add가 잘 실행되는지 확인
-    public void add(int input1, int input2) {
+    public void add() {
         calculator.add(2,3);
         assertThat(calculator.getResult(), is(5));
     }
 
     @Test //minus가 잘 실행되는지 확인
-    public void minus(int input1, int input2) {
+    public void minus() {
         calculator.minus(5, 2);
         assertThat(calculator.getResult(), is(3));
     }
 
     @Test // multiply가 잘 실행되는지 확인
-    public void multiply(int input1, int input2) {
+    public void multiply() {
         calculator.multiply(2,3);
         assertThat(calculator.getResult(), is(6));
     }
 
     @Test // divide가 잘 실행되는지 확인인
-    public void divide(int input1, int input2) {
+    public void divide() {
         calculator.divide(9, 3);
         assertThat(calculator.getResult(), is(3));
     }
 
-    @Test
+    @Test // 부피 구하기
     public void temp(){
-        
+        calculator.volume(3, 3, 3);
+        assertThat(calculator.getResult(), is(27));
+
+        calculator.volume(5);
+        assertThat(calculator.getAnswer(), is(78.5));
     }
 }
